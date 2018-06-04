ec80f1075fcfcad1825729c1329f8eb823763782
Cameron Leger <contact@cameronleger.com>
2018-06-04 17:55:14 -0600

    Add support for randomizing multiple pages

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Randomizer Bitwig Controller Script
-Randomizes all values on the Selected Devices' Selected Preset Page +/- a specified amount.
+# Randomizer Bitwig Controller Scripts
+## Randomizer
+Randomizes all values on the Selected Devices' Preset Page +/- a specified amount. It starts at the currently selected Preset Page and continues for n-1 more Preset Pages where n is another specified amount.
--- Scripts/Randomizer.control.js
+++ Scripts/Randomizer.control.js
@@ -5,20 +5,36 @@ host.defineController('Scripts', 'Randomizer', '1.0', '2244C2BD-912B-4047-8226-D
 
 var parameterValues = initArray(-1, 8);
 var parameterExists = initArray(false, 8);
+var parameterPage = -1;
+var parameterPages = -1;
+
+var isRandomizing = false;
+var originalPageIndex = -1;
+var lastPage = -1;
 
 function init()
 {
    doc = host.getDocumentState();
 
    randomizeRange = doc.getNumberSetting('Amount', 'Settings', 0, 100, 1, '%', 10);
+   randomizePages = doc.getNumberSetting('RC Pages', 'Settings', 0, 100, 1, 'Page(s)', 1);
    randomizeSignal = doc.getSignalSetting('Apply To', 'Actions', 'Selected Device');
 
-   randomizeSignal.addSignalObserver(randomize);
+   randomizeSignal.addSignalObserver(randomizeStart);
 
    cursorTrack = host.createCursorTrack('Selected Track', 'Selected Track', 1, 1, true);
 
    selectedDevice = cursorTrack.createCursorDevice();
    selectedDevicePage = selectedDevice.createCursorRemoteControlsPage(8);
+
+   selectedDevicePage.selectedPageIndex().addValueObserver(function(newValue) {
+      parameterPage = newValue;
+      if(isRandomizing) randomizeAgain();
+   });
+
+   selectedDevicePage.pageNames().addValueObserver(function(newValue) {
+      parameterPages = newValue.length;
+   });
 
    for(var p=0; p<8; p++)
    {
@@ -40,14 +56,48 @@ function makeIndexedFunction(index, f)
    };
 }
 
-function randomize()
+function randomizeStart()
 {
+   originalPageIndex = parameterPage;
+   var rPages = (randomizePages.get()*100);
+   if(rPages <= 0 || parameterPages === 0) return;
+   lastPage = Math.min(parameterPages - 1, parameterPage + (rPages - 1));
+   isRandomizing = true;
+   println('Randomizing pages ' + (parameterPage + 1) + ' - ' + (lastPage + 1) + ' (max: ' + parameterPages + ')');
+   randomizeAgain();
+}
+
+function randomizeNext()
+{
+   selectedDevicePage.selectNextPage(false);
+}
+
+function randomizeAgain()
+{
+   randomizer();
+   if(parameterPage >= lastPage)
+      randomizeFinish();
+   else
+      randomizeNext();
+}
+
+function randomizeFinish()
+{
+   println('Done!');
+   isRandomizing = false;
+   if(parameterPage !== originalPageIndex)
+      selectedDevicePage.selectedPageIndex().set(originalPageIndex);
+}
+
+function randomizer()
+{
+   println('Working on Remote Control Page #' + (parameterPage + 1));
+   r = randomizeRange.get() * 100;
    for(var p=0; p<8; p++)
    {
-      var param = selectedDevicePage.getParameter(p);
       if (parameterExists[p])
       {
-         r = randomizeRange.get() * 100;
+         var param = selectedDevicePage.getParameter(p);
          a = (Math.random() * r * 2) - r;
          v = parameterValues[p] + a;
          println('Setting parameter ' + p + ' to ' + v + ' from ' + parameterValues[p] + ' (' + a + ')');
